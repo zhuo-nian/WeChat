c9e8dc4a4c35fb177fd575cc64df5630ac3514d2
zuonians <zts63590@gmail.com>
2025-09-01 01:16:20 +0800

    '获取accession_token'

--- wechat/accessToken.js
+++ wechat/accessToken.js
@@ -1,4 +1,4 @@
-const { appId, appsecret } = require('../config');
+const { appID, appsecret } = require('../config');
 const axios = require('axios');
 
 class WeChat {
@@ -8,15 +8,18 @@ class WeChat {
     }
 
     getAccessToken() {
-        const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appId}&secret=${appsecret}`;
+        const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appID}&secret=${appsecret}`;
         return axios.get(url).then(res => {
-            console.log(res.data);
+            console.log('正确：', res.data);
             return res.data;
         }).catch(err => {
             console.log(err);
-            return Promise.reject(err);
+            return Promise.reject('错误：', err);
         });
 
     }
 
 }
+
+const weChat = new WeChat();
+weChat.getAccessToken();
