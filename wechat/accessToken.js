b849211ab89949e23e3b92a91c102bec3a29019b
zuonians <zts63590@gmail.com>
2025-09-01 10:27:45 +0800

    小更改

--- wechat/accessToken.js
+++ wechat/accessToken.js
@@ -10,11 +10,11 @@ class WeChat {
     getAccessToken() {
         const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appID}&secret=${appsecret}`;
         return axios.get(url).then(res => {
-            console.log('正确：', res.data);
+            console.log('获取accession——token成功：', res.data);
             return res.data;
         }).catch(err => {
             console.log(err);
-            return Promise.reject('错误：', err);
+            return Promise.reject('获取accession——token失败：', err);
         });
 
     }
