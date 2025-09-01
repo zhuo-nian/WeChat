cf55ea754370969a10c4f04ae50bb5764cdf13ba
zuonians <zts63590@gmail.com>
2025-09-02 00:56:27 +0800

    fetchAccessToken方法的实现

--- wechat/accessToken.js
+++ wechat/accessToken.js
@@ -1,5 +1,7 @@
-const { appID, appsecret } = require('../config');
 const axios = require('axios');
+const fs = require('fs').promises
+
+const { appID, appsecret } = require('../config');
 
 class WeChat {
 
@@ -7,19 +9,61 @@ class WeChat {
 
     }
 
-    getAccessToken() {
+    async getAccessToken() {
         const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appID}&secret=${appsecret}`;
-        return axios.get(url).then(res => {
-            console.log('获取accession——token成功：', res.data);
-            return res.data;
-        }).catch(err => {
-            console.log(err);
-            return Promise.reject('获取accession——token失败：', err);
-        });
-
+        try {
+            const res = await axios.get(url)
+            console.log('获取accessionToken成功：', res.data);
+            res.data.expires_in = Date.now() + (res.data.expires_in - 20) * 1000;
+            return res.data
+        } catch (error) {
+            console.log('获取accessionToken失败：', error);
+            throw error
+        }
     }
 
-}
+    async saveAccessToken(accessToken) {
+        try {
+            await fs.writeFile('./accessToken.txt', JSON.stringify(accessToken));
+            console.log('文件保存成功~');
+        } catch (error) {
+            console.log('获取accessionToken失败：', error);
+            throw error
+        }
+    }
 
-const weChat = new WeChat();
-weChat.getAccessToken();
+    async readAccessToken() {
+        try {
+            const data = await fs.readFile('./accessToken.txt');
+            console.log('文件读取成功~')
+            return JSON.parse(data);
+        } catch (error) {
+            console.log('readAccessToken出了问题：', error);
+            throw error
+        }
+    }
+
+    isValidAccessToken(data) {
+        if (!data || !data.access_token || !data.expires_in) {
+            return false;
+        }
+        return data.expires_in > Date.now();
+    }
+
+    async fetchAccessToken() {
+        try {
+            const data = await this.readAccessToken();
+            if (this.isValidAccessToken(data)) {
+                return data.access_token
+            } else {
+                data = await this.getAccessToken();
+                await this.saveAccessToken(data);
+                return data.access_token
+            }
+        } catch (error) {
+            const data = await this.getAccessToken();
+            await this.saveAccessToken(data);
+            return data.access_token
+        }
+    }
+}
