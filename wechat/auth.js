ef9042ec78462f3012068b97799a910da26f8bfe
zuonians <zts63590@gmail.com>
2025-09-02 23:22:18 +0800

    简单的自动回复

--- wechat/auth.js
+++ wechat/auth.js
@@ -20,8 +20,20 @@ module.exports = () => {
             const xmlData = await getUserDataAsync(req);
             const jsData = await parseXMLAsync(xmlData);
             const message = formatMessage(jsData);
-            console.log(message);
-            res.end('success');
+            let content = '你说的 ' + message.Content + ' 太复杂了，我还不太懂。';
+            if (message.MsgType === 'text') {
+                if (message.Content === '每日新闻') {
+                    content = '我将发送每日新闻图片';
+                }
+            }
+            let replyMessage = `<xml>
+                <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
+                <FromUserName><![CDATA[${message.ToUserName}]]></FromUserName>
+                <CreateTime>${Date.now()}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[${content}]]></Content>
+                </xml>`;
+            res.send(replyMessage);
         } else {
             res.end('error');
         }
