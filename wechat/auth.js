fad4195157ed490cd3df8de1d02229e46e81436a
zuonians <zts63590@gmail.com>
2025-09-05 17:04:53 +0800

    测试

--- wechat/auth.js
+++ wechat/auth.js
@@ -36,6 +36,7 @@ module.exports = () => {
             options.content = content;
             const replyMessage = template(options);
             console.log(replyMessage);
+            res.set('Content-Type', 'text/xml')
             res.send(replyMessage);
         } else {
             res.end('error');
